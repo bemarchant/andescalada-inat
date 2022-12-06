4b353999db995bb6aca7630fa7b8215a48bb9f81
bemarchant <bemarchant@uc.cl>
2022-12-06 10:26:37 -0300

    feat(app): Created new kingdom icon function

--- components/KingdomCard.tsx
+++ components/KingdomCard.tsx
@@ -1,4 +1,8 @@
 import { View, Text, StyleSheet, Pressable } from "react-native";
+import { KINGDOM } from "../utils/Constants";
+import AnimalKingdomIcon from "./svg/AnimalKingdomIcon";
+import FungiKingdomIcon from "./svg/FungiKingdomIcon";
+import PlantaeKingdomIcon from "./svg/PlantaeKingdomIcon";
 const MIN_HEIGHT = 80;
 
 const kingdomOnPressHandler = (text) => {
@@ -8,16 +12,24 @@ const kingdomOnPressHandler = (text) => {
 const KingdomCard = ({ kingdom, children, total_count }) => {
   let height = total_count < MIN_HEIGHT ? MIN_HEIGHT : total_count;
   height = MIN_HEIGHT;
+  console.log(kingdom.id)
   return (
     <View>
       <Pressable onPress={kingdomOnPressHandler.bind(this, children)}>
+
         <View
           style={[
             styles.kingdomContainer,
-            { backgroundColor: kingdom.color, height: height },
+            { backgroundColor: kingdom.color, height: 50 },
           ]}
         >
+          <View style={styles.kingdomIcon}>
+            {getKingdomIcon(kingdom.id)}
+          </View>
+
           <Text style={styles.kingdomText}> {total_count} </Text>
+
+
         </View>
       </Pressable>
     </View>
@@ -26,6 +38,18 @@ const KingdomCard = ({ kingdom, children, total_count }) => {
 
 export default KingdomCard;
 
+
+const getKingdomIcon = (taxa_id) => {
+  if(taxa_id === KINGDOM.animalia.id ){
+    return <AnimalKingdomIcon/>;
+  }
+  else if(taxa_id === KINGDOM.plantae.id){
+    return <PlantaeKingdomIcon />;
+  }
+  else if(taxa_id === KINGDOM.funga.id)
+    return <FungiKingdomIcon />;
+}
+
 const styles = StyleSheet.create({
   kingdomContainer: {
     flexDirection: "row",
@@ -33,9 +57,9 @@ const styles = StyleSheet.create({
     borderColor: "black",
     backgroundColor: "pink",
     borderWidth: 0,
-    borderRadius: 20,
+    borderRadius: 25,
     alignItems: "center",
-    justifyContent: "center",
+    justifyContent: "flex-start",
   },
 
   kingdomText: {
@@ -43,4 +67,11 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     color: "#454545ff",
   },
+
+
+  kingdomIcon: {
+    // backgroundColor: 'red',
+
+  },
+  
 });
