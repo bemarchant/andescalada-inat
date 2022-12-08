bf188378b0ffdd6b69d5242dcacbc21c782eb925
bemarchant <bemarchant@uc.cl>
2022-12-08 19:25:26 -0300

    fix(app): Avoid warning "cannot update a component"

--- screen/FieldGuideScreen.js
+++ screen/FieldGuideScreen.js
@@ -1,3 +1,4 @@
+import { useLayoutEffect } from "react";
 import { Dimensions, View, StyleSheet } from "react-native";
 import { WildLifeCard } from "../components/WildLifeCard";
 import { WILD_LIFE_DATA } from "../utils/Constants";
@@ -8,8 +9,12 @@ let WildLifeData = WILD_LIFE_DATA;
 const windowWidth = Dimensions.get("window").width;
 const windowHeight = Dimensions.get("window").height;
 
-const FieldGuideScreen = ({ route }) => {
-  for (let i = 0; i < MIN_OBS; i++) {}
+const FieldGuideScreen = ({ navigation, route }) => {
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: "Reino",
+    });
+  }, [navigation]);
 
   const observation = WILD_LIFE_DATA.find(
     (w) => w["taxaId"] === route.params.taxaId
