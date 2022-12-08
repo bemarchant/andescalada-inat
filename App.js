4b1acdf692be58a7246db58dd16d7d3aa2e6e943
bemarchant <bemarchant@uc.cl>
2022-12-08 12:45:33 -0300

    fix(app): Nested navigation into queryclientprovider

--- App.js
+++ App.js
@@ -1,22 +1,23 @@
 import { NavigationContainer, StackActions } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { View, StyleSheet } from "react-native";
+import { StyleSheet } from "react-native";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import MainScreen from "./screen/MainScreen";
 import FieldGuideScreen from "./screen/FieldGuideScreen";
 
-let screen = <MainScreen />;
-//screen = <FieldGuideScreen />;
-
+const queryClient = new QueryClient();
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="MainScreen" component={MainScreen} />
-        <Stack.Screen name="FieldGuideScreen" component={FieldGuideScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <QueryClientProvider client={queryClient}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="MainScreen" component={MainScreen} />
+          <Stack.Screen name="FieldGuideScreen" component={FieldGuideScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </QueryClientProvider>
   );
 }
 
