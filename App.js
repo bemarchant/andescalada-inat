5cb4fcef09a5b6718977c2128829916584d0ccce
bemarchant <bemarchant@uc.cl>
2022-12-26 21:48:05 -0300

    refactor(app)

--- App.js
+++ App.js
@@ -1,56 +1,32 @@
-import { NavigationContainer, StackActions } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { StyleSheet } from "react-native";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import MainScreen from "./screen/MainScreen";
-import FieldGuideScreen from "./screen/FieldGuideScreen";
-import EditWildLifeCardScreen from "./screen/EditWildLifeCardScreen";
-
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { WildLifeCardEditScreen } from "./screens/WildLifeCardEditScreen";
+import PopMenuContextProvider from "./store/context/popMenu-context";
 const queryClient = new QueryClient();
+
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <NavigationContainer>
-        <Stack.Navigator
-          screenOptions={{
-            headerStyle: { backgroundColor: "#454356ff" },
-            headerTintColor: "white",
-            contentStyle: { backgroundColor: "#454545ff" },
-          }}
-        >
-          <Stack.Screen
-            name="MainScreen"
-            component={MainScreen}
-            options={{
-              title: "El Manzano",
+        <PopMenuContextProvider>
+          <Stack.Navigator
+            screenOptions={{
+              headerStyle: { backgroundColor: "white" },
+              headerTintColor: "gray",
+              contentStyle: { backgroundColor: "black" },
             }}
-          />
-          <Stack.Screen
-            name="FieldGuideScreen"
-            component={FieldGuideScreen}
-            options={{
-              title: "El Manzano",
-            }}
-          />
-
-          <Stack.Screen
-            name="EditWildLifeCardScreen"
-            component={EditWildLifeCardScreen}
-            options={{
-              title: "El Manzano",
-            }}
-          />
-        </Stack.Navigator>
+          >
+            <Stack.Screen
+              name="WildLifeCardEditScreen"
+              component={WildLifeCardEditScreen}
+              options={{ title: "Editor" }}
+            />
+          </Stack.Navigator>
+        </PopMenuContextProvider>
       </NavigationContainer>
     </QueryClientProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  screenContainer: {
-    flex: 1,
-    backgroundColor: "#454545ff",
-  },
-});
