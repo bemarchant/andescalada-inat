7d3d29994d423160498ddcca68e90857c12ab8ba
bemarchant <bemarchant@uc.cl>
2022-12-27 07:21:39 -0300

    refactor(app): moved kingdom icons to icons folder

---
+++ components/icons/AnimalKingdomIcon.js
@@ -0,0 +1,216 @@
+import * as React from "react";
+import Svg, { G, Circle, Path } from "react-native-svg";
+
+function AnimalKingdomIcon(props) {
+  return (
+    <Svg
+      width="52.328999mm"
+      height="52.328999mm"
+      viewBox="0 0 52.328999 52.328999"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <G fill="#805b47" fillOpacity={1}>
+        <Circle
+          cx={-86.029007}
+          cy={43.820213}
+          r={43.005566}
+          transform="matrix(.59024 0 0 .59024 76.942 .3)"
+          fill="#805b47"
+          fillOpacity={1}
+          stroke="#f8fafb"
+          strokeWidth={2.64583}
+          strokeLinejoin="bevel"
+          strokeDasharray="none"
+          strokeOpacity={1}
+          stopColor="#000"
+        />
+      </G>
+      <Path
+        d="M128.169-89.309l-1.002-8.79 3.4-4.058 3.371-4.778 3.29-.986 1.546-3.718-20.09-11.725-1.166.311-1.799.48z"
+        transform="translate(-92.868 104.538) scale(.80688)"
+        display="inline"
+        fill="#d5a169"
+        fillOpacity={1}
+        stroke="none"
+        strokeWidth=".112803px"
+        strokeLinecap="butt"
+        strokeLinejoin="miter"
+        strokeOpacity={1}
+      />
+      <Path
+        d="M128.169-89.309l-12.45-33.264 1.8-.48 19.708 15.132-3.289.986-4.974 5.02z"
+        transform="translate(-92.868 104.538) scale(.80688)"
+        display="inline"
+        fill="#d8b68d"
+        fillOpacity={1}
+        stroke="none"
+        strokeWidth=".112803px"
+        strokeLinecap="butt"
+        strokeLinejoin="miter"
+        strokeOpacity={1}
+      />
+      <Path
+        d="M166.874-89.309l1.002-8.79-3.4-4.058-3.371-4.778-3.29-.986-1.546-3.718 20.09-11.725 1.166.311 1.799.48z"
+        transform="translate(-92.868 104.538) scale(.80688)"
+        display="inline"
+        fill="#d5a169"
+        fillOpacity={1}
+        stroke="none"
+        strokeWidth=".112803px"
+        strokeLinecap="butt"
+        strokeLinejoin="miter"
+        strokeOpacity={1}
+      />
+      <Path
+        d="M166.874-89.309l12.45-33.264-1.799-.48-19.71 15.132 3.29.986 4.974 5.02z"
+        transform="translate(-92.868 104.538) scale(.80688)"
+        display="inline"
+        fill="#ceaa7f"
+        fillOpacity={1}
+        stroke="none"
+        strokeWidth=".112803px"
+        strokeLinecap="butt"
+        strokeLinejoin="miter"
+        strokeOpacity={1}
+      />
+      <Path
+        d="M147.521-113.268l-4.746.261-4.001 1.368-1.547 3.718-3.289.986-2.319.292-1.052 1.324-3.4 7.22 1.002 10.889 6.994 5.294 5.046 6.965 1.258 1.098 3.12 2.722 2.934.233 2.935-.233 3.12-2.722 1.258-1.098 5.046-6.965 6.994-5.294 1.002-10.89-3.4-7.219-1.213-.581-2.158-1.035-3.29-.986-1.546-3.718-4.001-1.368z"
+        transform="translate(-92.868 104.538) scale(.80688)"
+        display="inline"
+        fill="#d5a169"
+        fillOpacity={1}
+        stroke="none"
+        strokeWidth={0.451209}
+        strokeLinecap="butt"
+        strokeLinejoin="miter"
+        strokeMiterlimit={4}
+        strokeDasharray="none"
+        strokeOpacity={1}
+      />
+      <Path
+        d="M147.522-113.268v42.37l2.934-.233 4.378-3.82 5.046-6.966 6.994-5.293 1.002-10.89-3.4-7.219-3.371-1.616-3.29-.986-1.546-3.718-4.001-1.368z"
+        transform="translate(-92.868 104.538) scale(.80688)"
+        fill="#b8734e"
+        fillOpacity={1}
+        strokeWidth={0.132292}
+        strokeLinejoin="bevel"
+        stopColor="#000"
+      />
+      <Path
+        d="M147.522-74.531l-3.303.956-2.752-.278-1.258-1.098-.792-4.983.42-1.91.533-7.139 7.152-4.172z"
+        transform="translate(-92.868 104.538) scale(.80688)"
+        fill="#ebd9c4"
+        fillOpacity={1}
+        stroke="none"
+        strokeWidth={0.132292}
+        strokeLinejoin="bevel"
+        strokeOpacity={1}
+        stopColor="#000"
+      />
+      <Path
+        d="M147.522-74.531l3.302.956 2.752-.278 1.258-1.098.792-4.983-.42-1.91-.533-7.139-7.151-4.172z"
+        transform="translate(-92.868 104.538) scale(.80688)"
+        fill="#ceaa7f"
+        fillOpacity={1}
+        stroke="none"
+        strokeWidth={0.132292}
+        strokeLinejoin="bevel"
+        strokeOpacity={1}
+        stopColor="#000"
+      />
+      <Path
+        d="M156.27-111.639l1.546 3.718 19.71-15.132-1.166-.31z"
+        transform="translate(-92.868 104.538) scale(.80688)"
+        fill="#b8734e"
+        fillOpacity={1}
+        stroke="none"
+        strokeWidth={0.132292}
+        strokeLinejoin="bevel"
+        stopColor="#000"
+      />
+      <Path
+        d="M147.521-82.44l-2.54.13-.943 2.09.985 2.605 2.498.465 2.5-.465.984-2.605-.943-2.09z"
+        transform="translate(-92.868 104.538) scale(.80688)"
+        display="inline"
+        fill="#24292d"
+        fillOpacity={1}
+        stroke="none"
+        strokeWidth={0.416376}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeMiterlimit={4}
+        strokeDasharray="none"
+        strokeOpacity={1}
+      />
+      <Path
+        d="M126.118-103.387l-1.276-7.182.112-3.279-4.475-4.808-.957-.32-.48 1.597z"
+        transform="translate(-92.868 104.538) scale(.80688)"
+        display="inline"
+        fill="#d5a169"
+        fillOpacity={1}
+        stroke="none"
+        strokeWidth={0.515373}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeMiterlimit={4}
+        strokeDasharray="none"
+        strokeOpacity={1}
+      />
+      <Path
+        d="M168.925-103.387l1.277-7.182-.113-3.279 4.475-4.808.958-.32.478 1.597z"
+        transform="translate(-92.868 104.538) scale(.80688)"
+        display="inline"
+        fill="#b8734e"
+        fillOpacity={1}
+        stroke="none"
+        strokeWidth={0.515373}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeMiterlimit={4}
+        strokeDasharray="none"
+        strokeOpacity={1}
+      />
+      <Path
+        d="M159.86-94.1l-.93 1.77-.793.605-4.126.521-1.623-1.128 1.455-2.504 3.277-.361zM135.182-94.1l.932 1.77.792.605 4.126.521 1.623-1.128-1.455-2.504-3.277-.361z"
+        transform="translate(-92.868 104.538) scale(.80688)"
+        display="inline"
+        fill="#24292d"
+        fillOpacity={1}
+        stroke="none"
+        strokeWidth={0.451209}
+        strokeLinecap="butt"
+        strokeLinejoin="miter"
+        strokeMiterlimit={4}
+        strokeDasharray="none"
+        strokeOpacity={1}
+      />
+      <Path
+        d="M140.209-74.95l1.258 1.097 2.752.278 3.303-.956"
+        transform="translate(-92.868 104.538) scale(.80688)"
+        fill="none"
+        fillOpacity={1}
+        stroke="#b8734e"
+        strokeWidth={0.451115}
+        strokeLinejoin="bevel"
+        strokeDasharray="none"
+        strokeOpacity={1}
+        stopColor="#000"
+      />
+      <Path
+        d="M147.522-74.531l3.302.956 2.752-.278 1.258-1.098"
+        transform="translate(-92.868 104.538) scale(.80688)"
+        fill="none"
+        fillOpacity={1}
+        stroke="#b8734e"
+        strokeWidth={0.451115}
+        strokeLinejoin="bevel"
+        strokeDasharray="none"
+        strokeOpacity={1}
+        stopColor="#000"
+      />
+    </Svg>
+  );
+}
+
+export default AnimalKingdomIcon;
